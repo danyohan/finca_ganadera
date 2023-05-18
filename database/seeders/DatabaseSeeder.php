b35fda5867af23c6a790bc2c2290fdc483952206
Dany Perez <danyohan10@hotmail.com>
2023-05-17 22:22:28 -0500

    feat: The seeders were implemented

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\Animal;
+use App\Models\AnimalType;
+use App\Models\Color;
+use App\Models\illness;
+use App\Models\Illnesses_detail;
+use App\Models\Paddock;
+use App\Models\Race;
+use App\Models\Status;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,11 +21,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       Status::factory(4)->create();
+       Color::factory(4)->create();
+       Race::factory(4)->create();
+       Illness::factory(4)->create();
+       Paddock::factory(4)->create();
+       AnimalType::factory(4)->create();
+       Animal::factory(4)->create();
+       Illnesses_detail::factory(4)->create();
+      
     }
 }
